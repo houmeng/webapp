7cea3c71a6c3b5f934fd695a268587bea2c51dbc
meng hou <hou.m@neusoft.com>
2016-09-02 17:43:32 +0800

    Create front end pages.

--- www/handlers.py
+++ www/handlers.py
@@ -9,13 +9,18 @@ __author__ = "Meng Hou"
 
 from framework import *
 import asyncio
-from models import User
+from models import User,Blog
 
 @get("/")
 def index(request):
-    users = yield from User.findall()
+    summary = "this is for test"
+    blogs = [
+        Blog(id="1", name="Test Blog", summary=summary, created_at=time.time() - 120),
+        Blog(id="2", name="Test Blog2", summary=summary, created_at=time.time() - 3600),
+        Blog(id="3", name="Test Blog3", summary=summary, created_at=time.time() - 7200),
+    ]
 
     return {
-        "__template__": "test.html",
-        "users": users
+        "__template__": "blogs.html",
+        "blogs": blogs
     }
